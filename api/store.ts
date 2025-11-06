13c394d77b684ba9a757043583e5acad815b41ac
MignonErasmus <u22492586@tuks.co.za>
2025-11-06 10:06:17 +0200

    added overall educator dashboards stats functionality and updated the metric card styling

--- api/store.ts
+++ api/store.ts
@@ -1,14 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { finalQuizApi } from "@/api/services/FinalQuiz"
 import { educatorDashboardApi } from "@/api/services/EducatorDashboardStudentPerformanceSummary"
+import { educatorDashboardStatsApi } from "@/api/services/EducatorDashboardOverallStats" 
 
 export const store = configureStore({
   reducer: {
     [finalQuizApi.reducerPath]: finalQuizApi.reducer,
     [educatorDashboardApi.reducerPath]: educatorDashboardApi.reducer,
+    [educatorDashboardStatsApi.reducerPath]: educatorDashboardStatsApi.reducer,
   },
   middleware: (getDefault) =>
-    getDefault().concat(finalQuizApi.middleware, educatorDashboardApi.middleware),
+    getDefault().concat(
+      finalQuizApi.middleware,
+      educatorDashboardApi.middleware,
+      educatorDashboardStatsApi.middleware
+    ),
 })
 
 export type RootState = ReturnType<typeof store.getState>
